b5b77afa8816f0ee21fc3776d9636a7231c53fa6
Marc Jammet <marcjammet@mac.com>
2025-08-07 22:09:30 +0200

    Defer React app mount until DOM content is fully loaded

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
-ReactDOM.createRoot(document.getElementById('app')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-)
+const mount = () => {
+  ReactDOM.createRoot(document.getElementById('app')!).render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  )
+}
+
+if (document.readyState === 'loading') {
+  document.addEventListener('DOMContentLoaded', mount)
+} else {
+  mount()
+}

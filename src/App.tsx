aa0e73c2872f44b4af51c7507ef7fbaca39acd4e
Marc Jammet <marcjammet@mac.com>
2025-08-07 22:36:11 +0200

    Update App.tsx to improve unauthenticated user handling and remove unused AssetDetailPage route

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,24 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import { getUser } from './auth'
 import { AuthPage } from './AuthPage'
 import { ExplorerPage } from './ExplorerPage'
-import { AssetDetailPage } from './AssetDetailPage'
 import { Layout } from './Layout'
 
 export default function App() {
   const user = getUser()
-  if (!user) return <Routes><Route path="*" element={<Navigate to="/login" replace />} /></Routes>
+  console.log("Utilisateur détecté :", user)
+
+  if (!user) {
+    return (
+      <Routes>
+        <Route path="*" element={<AuthPage />} />
+      </Routes>
+    )
+  }
 
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<ExplorerPage />} />
-        <Route path="/asset/:id" element={<AssetDetailPage />} />
         <Route path="/login" element={<AuthPage />} />
       </Routes>
     </Layout>
